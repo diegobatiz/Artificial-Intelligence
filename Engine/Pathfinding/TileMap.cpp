8b37afb33dafc9aef58eeb75cf8838aee8da6735
diegobatiz <diegow.batiz@gmail.com>
2023-07-06 21:49:25 -0700

    getting started

---
+++ Engine/Pathfinding/TileMap.cpp
@@ -0,0 +1,37 @@
+#include "TileMap.h"
+
+namespace
+{
+	inline int ToIndex(int x, int y, int columns)
+	{
+		return x + (y * columns);
+	}
+}
+
+void TileMap::LoadTiles(const char* fileName)
+{
+	// TODO - Read the provided file and populate mTiles here
+}
+
+void TileMap::LoadMap(const char* fileName)
+{
+	// TODO - Read the provided file and populate mMap here
+}
+
+void TileMap::Render() const
+{
+	// TODO - Draw the map using mTiles and mMap
+}
+
+// 2D map - 5 columns x 4 rows
+// [0][0][0][0][0]
+// [0][0][0][0][0]
+// [0][0][0][X][0]   X is at (3, 2)
+// [0][0][0][0][0]
+
+// Stored as 1D - 5x4 = 20 slots
+// [0][0][0][0][0] [0][0][0][0][0] [0][0][0][X][0] [0][0][0][0][0]
+//
+// index = column + (row * columnCount)
+//       = 3 + (2 * 5)
+//       = 13
